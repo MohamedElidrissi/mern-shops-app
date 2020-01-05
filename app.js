27e922e86ac3dc862dea2c946c3c02b2c17e30e8
Mohamed ELIDRISSI <45264848+MohamedElidrissi@users.noreply.github.com>
2020-01-05 13:22:55 +0100

    Setup mongoose connection

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const logger = require('morgan');
+const debug = require('debug')('mern-shops-app:server');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const Router = require('./routes/');
 
 dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.once('open', () => debug('Connected to MongoDB!'));
+db.on('error', debug.bind(debug, 'Could not connect to MongoDB:'));
 
 app.use(logger('dev'));
 app.use(cors());
