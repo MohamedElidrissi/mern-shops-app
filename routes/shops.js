6389d9062be7966b1b7071d43b537511d73a322c
Mohamed ELIDRISSI <45264848+MohamedElidrissi@users.noreply.github.com>
2020-01-07 16:45:22 +0100

    Require authentication before showing all shops

--- routes/shops.js
+++ routes/shops.js
@@ -1,6 +1,7 @@
 const { show } = require('../controllers/shop/');
 const { getAllShopsValidator } = require('../middlewares/validation/');
+const passport = require('../middlewares/passport/');
 
 module.exports = router => {
-  router.get('/shops', getAllShopsValidator, show);
+  router.get('/shops', [getAllShopsValidator, passport('jwt')], show);
 };
